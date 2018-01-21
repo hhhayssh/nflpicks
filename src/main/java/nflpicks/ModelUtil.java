4d76e82680764788c0d5a879a34a3e675d003315
Al Bundy <albundy@bundy.com>
2018-01-20 23:48:07 -0500

    Makes it so we can make a runnable jar to do the export of picks so we can do an export from the command line pretty easily.

--- src/main/java/nflpicks/ModelUtil.java
+++ src/main/java/nflpicks/ModelUtil.java
@@ -8,23 +8,47 @@ import nflpicks.model.Pick;
 import nflpicks.model.Player;
 import nflpicks.model.Record;
 
+/**
+ * 
+ * Here to hold "utility" functions for working with different kinds of model objects.
+ * Here so that the "utility" functions for different model classes can be referenced
+ * in one place and so they can easily refer to each other without needing an instance.
+ * 
+ * @author albundy
+ *
+ */
 public class ModelUtil {
 	
+	/**
+	 * 
+	 * Gets the pick that the given player name for the given game from the given list.
+	 * If it doesn't find the game in the list of picks or the player didn't make a pick
+	 * for the game, it'll return null.
+	 * 
+	 * @param picks
+	 * @param playerName
+	 * @param gameId
+	 * @return
+	 */
 	public static Pick getPick(List<Pick> picks, String playerName, int gameId){
 		
-		if (picks == null){
+		//Steps to do:
+		//	1. Go through all the picks and look for ones made by the given player.
+		//	2. If we find one that's for the game and was picked by the given player,
+		//	   that's the one we're looking for.
+		//	3. If we go through it all, that means the pick doesn't exist in the list.
+		
+		if (picks == null || playerName == null){
 			return null;
 		}
 		
 		for (int index = 0; index < picks.size(); index++){
 			Pick pick = picks.get(index);
 			
+			int currentGameId = pick.getGame().getId();
 			String currentPlayerName = pick.getPlayer().getName();
-			if (!playerName.equals(currentPlayerName)){
-				continue;
-			}
 			
-			if (pick.getGame().getId() == gameId){
+			if (gameId == currentGameId && playerName.equals(currentPlayerName)){
 				return pick;
 			}
 		}
